d666d3e72ce858e2fc2f9c7d0d440f86a3b39d32
EC2 Default User <ec2-user@ip-172-31-59-14.ec2.internal>
2015-08-22 19:46:51 +0000

    change assets to compile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,8 +28,9 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # Fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = true
+  config.assets.precompile =  ['*.js', '*.css', '*.css.erb'] 
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
