4e9740ed601d47dbe241698ae16917b30f27baa2
emthomas <titbabthomas@hotmail.com>
2015-08-25 08:51:41 -0400

    try fixing tests

--- spec/requests/spendings_spec.rb
+++ spec/requests/spendings_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Spendings", type: :request do
   describe "GET /spendings" do
     it "works! (now write some real specs)" do
       get spendings_path
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(302)
     end
   end
 end
