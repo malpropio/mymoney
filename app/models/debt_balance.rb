83378bb606011217f8a45fbd7f960b96d0ebda4b
Rails <titbabthomas@gmail.com>
2015-09-13 00:19:16 +0000

    fix debt balance creation

--- app/models/debt_balance.rb
+++ app/models/debt_balance.rb
@@ -20,17 +20,19 @@ class DebtBalance < ActiveRecord::Base
 
   private
   def budget_not_set_for_month
-    errors.add(:debt, "balance already set for #{self.due_date.strftime('%B %Y')}") if DebtBalance.where("id != #{self.id} AND debt_id = #{self.debt_id} AND DATE_FORMAT(due_date, '%Y-%m-%01') = DATE_FORMAT('#{self.due_date}', '%Y-%m-%01')").exists? 
+    errors.add(:debt, "balance already set for #{self.due_date.strftime('%B %Y')}") if DebtBalance.where("id != #{self.id || 0} AND debt_id = #{self.debt_id} AND DATE_FORMAT(due_date, '%Y-%m-%01') = DATE_FORMAT('#{self.due_date}', '%Y-%m-%01')").exists? 
   end
 
   def start_pay_date
-    if DebtBalance.where("id != #{self.id} AND debt_id = #{self.debt_id} AND '#{self.payment_start_date}' <= due_date AND '#{self.due_date}' > due_date").exists?
-      previous = DebtBalance.where("id != #{self.id} AND debt_id = #{self.debt_id} AND '#{self.payment_start_date}' <= due_date AND '#{self.due_date}' > due_date").order(due_date:  "desc").first
+    if DebtBalance.where("id != #{self.id || 0} AND debt_id = #{self.debt_id} AND '#{self.payment_start_date}' <= due_date AND '#{self.due_date}' > due_date").exists?
+      previous = DebtBalance.where("id != #{self.id || 0} AND debt_id = #{self.debt_id} AND '#{self.payment_start_date}' <= due_date AND '#{self.due_date}' > due_date").order(due_date:  "desc").first
       errors.add(:payment_start_date, "must be after #{previous.due_date}")
     end
-
-    if self.due_date < self.payment_start_date
-      errors.add(:payment_start_date, "(#{self.payment_start_date}) must be before due date (#{self.due_date})")
+    
+    if !self.due_date.blank? && !self.payment_start_date.blank?
+      if self.due_date < self.payment_start_date
+        errors.add(:payment_start_date, "(#{self.payment_start_date}) must be before due date (#{self.due_date})")
+      end
     end
   end
 end
