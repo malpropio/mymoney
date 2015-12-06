9663814fc5e76cfa3b38de4eb40408c39ab23b71
emthomas <titbabthomas@gmail.com>
2015-12-05 21:51:02 -0500

    fix: estimated cc payment doesn't consider payments to date anymore

--- app/helpers/budgets_helper.rb
+++ app/helpers/budgets_helper.rb
@@ -26,7 +26,7 @@ module BudgetsHelper
     savings = DebtBalance.joins(:debt).where("debt_balances.payment_start_date<='#{date}' AND due_date>='#{date}' AND debts.sub_category = 'Credit Cards'")
     result = 0
     if savings.exists?
-      savings.each {|saving| result += saving.payment_due(date) * paychecks(saving.debt.pay_from,date)  }
+      savings.each {|saving| result += saving.payment_due(date, false) * paychecks(saving.debt.pay_from,date)  }
     end
     result
   end
@@ -35,7 +35,7 @@ module BudgetsHelper
     savings = DebtBalance.joins(:debt).where("debt_balances.payment_start_date<='#{date}' AND due_date>='#{date}' AND debts.sub_category = 'Credit Cards'")
     result = "Estimated payments needed to payoff all credit card balances: "
     if savings.exists?
-      savings.each {|saving| result += "#{saving.debt.name} => #{number_to_currency(saving.payment_due(date))} x #{paychecks(saving.debt.pay_from,date)}; "  }
+      savings.each {|saving| result += "#{saving.debt.name} => #{number_to_currency(saving.payment_due(date, false))} x #{paychecks(saving.debt.pay_from,date)}; "  }
     end
     result
   end
