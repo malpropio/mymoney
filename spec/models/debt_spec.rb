a1ea819f2ec6754d7e6012af66e9908e578aafea
Alain P <kazou83@yahoo.com>
2015-12-29 23:30:33 +0000

    TG-17 Add validity test

--- spec/models/debt_spec.rb
+++ spec/models/debt_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Debt, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+	expect(FactoryGirl.build(:debt)).to be_valid
+  end
+
+  it "is invalid without name" do
+	debt = FactoryGirl.build(:debt, name: nil)
+	expect(debt).to_not be_valid
+	expect(debt.errors).to have_key(:name)
+  end
+
+  it "is invalid without category" do
+	debt = FactoryGirl.build(:debt, category: nil)
+	expect(debt).to_not be_valid
+	expect(debt.errors).to have_key(:category_id)
+  end
+
+  it "has many debt balances" 
+	 
 end
