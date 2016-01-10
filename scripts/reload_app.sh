55d04bbe0d97cb715c72027d24455a8d16667f7d
Sebastien Thomas <titbabthomas@gmail.com>
2016-01-10 04:47:44 -0500

    Add slack notifications

--- scripts/reload_app.sh
+++ scripts/reload_app.sh
@@ -12,3 +12,6 @@ else
 fi
 
 passenger start /var/apps/mymoney
+
+#Slack Notification
+curl -X POST --data-urlencode 'payload={"text": "Deployment '${DEPLOYMENT_ID}' of '${APPLICATION_NAME}' on '${DEPLOYMENT_GROUP_NAME}' has completed"}' https://hooks.slack.com/services/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
